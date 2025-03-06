25ef77a0a57a729b372eb3432e2c94d568915219
Truce604 <JordanLilwall@gmail.com>
2025-03-06 00:40:27 -0700

    Update auth.js
    Fixed Firebase module errors by using Google's CDN

--- auth.js
+++ auth.js
@@ -1,11 +1,13 @@
-import { initializeApp } from "firebase/app";
+
 
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
 <<<<<<< HEAD
   // Import the functions you need from the SDKs you need
-
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
