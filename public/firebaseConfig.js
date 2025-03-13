6b7c89c93900b05d608f6c728e9d91f55f20f97c
Truce604 <JordanLilwall@gmail.com>
2025-03-13 13:55:16 -0600

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,14 +1,13 @@
 
-const firebaseConfig = {
+// ✅ Declare Firebase Config Globally
+window.firebaseConfig = {
     apiKey: "AIzaSyAGmhdeSxshYSmaAbsMtda4qa1K3TeKiYw",
     authDomain: "trackrepost-921f8.firebaseapp.com",
     projectId: "trackrepost-921f8",
     storageBucket: "trackrepost-921f8.appspot.com",
     messagingSenderId: "967836604288",
-    appId: "1:967836604288:web:3782d50de7384c9201d365",
+    appId: "1:967836604288:web:3782d50de7384c9201d365"
     measurementId: "G-G65Q3HC3R8"
 };
 
-// ✅ Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 console.log("✅ Firebase Config Loaded Successfully!");
