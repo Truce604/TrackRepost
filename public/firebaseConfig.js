632995fd2ec41dacb0ee13fa55320ebeb5a06bd6
Truce604 <JordanLilwall@gmail.com>
2025-03-13 18:15:52 -0600

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,19 +1,19 @@
 
+// ✅ Firebase Configuration 
 const firebaseConfig = {
     apiKey: "AIzaSyAGmhdeSxshYSmaAbsMtda4qa1K3TeKiYw",
     authDomain: "trackrepost-921f8.firebaseapp.com",
     projectId: "trackrepost-921f8",
     storageBucket: "trackrepost-921f8.appspot.com",
     messagingSenderId: "967836604288",
-    appId: "1:967836604288:web:3782d50de7384c9201d365"
+    appId: "1:967836604288:web:3782d50de7384c9201d365",
     measurementId: "G-G65Q3HC3R8"
 };
 
-// ✅ Ensure Firebase is initialized
+// ✅ Initialize Firebase
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
     console.log("✅ Firebase Initialized Successfully!");
 } else {
     console.log("⚠️ Firebase already initialized.");
 }
-
