073e52926a194de7a7e662dc122a7cd06342580f
Truce604 <JordanLilwall@gmail.com>
2025-03-14 16:11:33 -0600

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     projectId: "trackrepost-921f8", 
     storageBucket: "trackrepost-921f8.appspot.com", 
     messagingSenderId: "967836604288", 
-    appId: 1:967836604288:web:3782d50de7384c9201d365"", 
+    appId: "1:967836604288:web:3782d50de7384c9201d365", 
     measurementId: "G-G65Q3HC3R8" 
 };
 
