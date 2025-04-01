aa9e9f0ee4de2d9600c4dcbecdbcc3ca2ddba802
Truce604 <JordanLilwall@gmail.com>
2025-03-31 19:50:23 -0600

    Create firebaseConfig.js

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,30 @@
+// /public/firebaseConfig.js
+
+// ✅ Make Firebase config globally available
+window.firebaseConfig = {
+    apiKey: "AIzaSyAGmhdeSxshYSmaAbsMtda4qa1K3TeKiYw", 
+    authDomain: "trackrepost-921f8.firebaseapp.com", 
+    projectId: "trackrepost-921f8", 
+    storageBucket: "trackrepost-921f8.appspot.com", 
+    messagingSenderId: "967836604288", 
+    appId: "1:967836604288:web:3782d50de7384c9201d365", 
+    measurementId: "G-G65Q3HC3R8" 
+};
+
+// ✅ Square Credentials (can still be global)
+window.SQUARE_APP_ID = "EAAAl2fPk73oOW5y3brJgQkeICaFS_tGz0w5NrFmyhciQ5E_m8GeUbdYw4gDw-wE";
+window.SQUARE_LOCATION_ID = "sq0idp-PgaanSd67uGXtHuBFn7cZA";
+
+console.log("✅ Square App ID:", window.SQUARE_APP_ID);
+console.log("✅ Square Location ID:", window.SQUARE_LOCATION_ID);
+
+
+
+
+
+
+
+
+
+
+
